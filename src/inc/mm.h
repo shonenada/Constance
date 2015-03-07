4ce4e5b413ddd3e21e924c7185a11f534aea697d
shonenada <shonenada@gmail.com>
2015-03-07 08:13:39 +0000

    rename

--- src/inc/mm.h
+++ src/inc/mm.h
@@ -3,12 +3,18 @@
 
 #include <const.h>
 
+// set cr3 to be the base address of page directory
 extern void flush_cr3();
+// set cr0(PG) to the 1, enable paging
 extern void page_enable();
+
 void page_init();
+void kmalloc();
 
 #define PG_P 0b1
 #define PG_RW 0b10
 #define PG_U 0b100
+
+#define PAGE_SIZE (4*1024)     // 4kb each page
 
 #endif
---
+++ src/mm/memory.c
@@ -0,0 +1,23 @@
+#include <const.h>
+#include <page.h>
+
+uint *pdir = (uint *) 0x9C000;
+uint *ptab = (uint *) 0x9D000;
+
+// setup paging
+void page_init() {
+    int i;
+    uint addr = 0;
+    for(i=0;i<NR_PAGE;i++) {
+        ptab[i] = (addr | PG_P | PG_RW);
+        addr += PAGE_SIZE;    // 4kb each page
+    }
+    for(i=0;i<NR_PAGE;i++)
+        pdir[i] = 0 | PG_RW;
+
+    // first entry
+    pdir[0] = ((uint) ptab) | PG_P | PG_RW;
+
+    flush_cr3();
+    page_enable();
+}
