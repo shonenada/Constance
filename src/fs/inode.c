d94ab94e241ade6b503a20be6259e7a0f747be39
shonenada <shonenada@gmail.com>
2015-04-04 18:04:44 +0000

    update inode.c

--- src/fs/inode.c
+++ src/fs/inode.c
@@ -11,8 +11,8 @@
 struct inode inodes[NINODE];
 
 struct inode * iget(uint dev, uint num) {
-    struct inode *ip;
     struct sblk *sp;
+    struct inode *ip;
 
 _loop:
     for (ip=inodes; ip<inodes+NINODE; ip++) {
@@ -22,7 +22,6 @@ _loop:
                 sleep((uint) ip);
                 goto _loop;
             }
-
             if (ip->flag & I_MOUNT) {
                 for (sp=mnt;sp<mnt+NMOUNT;sp++) {
                     if (ip == sp->imnt) {
@@ -52,9 +51,9 @@ _loop:
 }
 
 int iload(struct inode *ip) {
+    struct buf *bp;
     struct sblk *sp;
     struct d_inode *inp;
-    struct buf *bp;
 
     sp = getsblk(ip->idev);
     if (sp == NULL) {
@@ -77,18 +76,18 @@ void iupdate(struct inode *ip) {
     if (sbp == NULL) {
         panic("iupdate: error super block");
     }
+    bp = buf_read(ip->idev, INOBLOCK(sbp, ip->inum));
 
-    bp = buf_read(ip->idev, IBLOCK(ip->inum));
 
     inp = (struct d_inode*) bp->data;
     memcpy(&inp[(ip->inum-1)%IPB], ip->zone, sizeof(struct d_inode));
     ip->flag &= ~I_DIRTY;
-    hd_sync(bp);
+    buf_write(bp);
+    buf_relse(bp);
 }
 
 int iput(struct inode *ip) {
     ushort dev;
-
     if (ip == NULL) {
         panic("iput: wrong inode");
     }
@@ -170,6 +169,52 @@ int unlink_inode(struct inode *ip) {
     }
     ip->flag &= ~(I_LOCK | I_WANTED);
     sti();
+    return 0;
+}
+
+// alloc inode
+int ialloc (ushort dev) {
+    uint i, ino;
+    int r;
+    struct sblk *sbp;
+    struct buf *bp;
+    sbp = getsblk(dev);
+
+    for (i=0;i<sbp->ninodes;i++) {
+        bp = buf_read(dev, INOBLOCK(sbp, i));
+        r = find_free(bp->data, BLK_SIZE);
+        if (r < 0) {
+            continue;
+        }
+        ino = i*BLK_SIZE + r;
+        bp->data[ino/8] |= 1 << (ino%8);
+        hd_sync(bp);
+        unlink_sb(sbp);
+        return ino;
+    }
+    unlink_sb(sbp);
+    panic("no enough inode");
+    return -1;
+}
+
+// free inode
+int ifree (ushort dev, uint ino) {
+    struct buf *bp;
+    struct sblk *sbp;
+    sbp = getsblk(dev);
+    if ((ino <= 0) || (ino >= sbp->ninodes)) {
+        panic("inode no exist");
+    }
+    bp = buf_read(dev, INOBLOCK(sbp, ino));
+    /**
+    if ((bp->data[ino/8] & (1 << (ino%8))) == 0) {
+        panic("inode is free");
+    }
+    */
+    bp->data[ino/8] &= ~(1<<(ino%8));
+    hd_sync(bp);
+    buf_relse(bp);
+    unlink_sb(sbp);
     return 0;
 }
 
