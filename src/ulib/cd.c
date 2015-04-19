efe8667972105a40f37c25dc988661a36b470f58
shonenada <shonenada@gmail.com>
2015-04-19 01:48:35 +0000

    add cd.c

---
+++ src/ulib/cd.c
@@ -0,0 +1,20 @@
+#include <const.h>
+#include <system.h>
+#include <fs.h>
+#include <file.h>
+#include <sched.h>
+#include <inode.h>
+#include <errno.h>
+
+int do_cd(char *path) {
+    struct inode* ip;
+    ip = namei(path, 0);
+    if (ip == NULL) {
+        syserr(ENOTDIR);
+        return -1;
+    }
+    current->wdir = ip;
+    iput(ip);
+    return 0;
+}
+
