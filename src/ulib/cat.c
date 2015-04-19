2593dcc3a57ca0add66a2e128a51e3bccabdeaa3
shonenada <shonenada@gmail.com>
2015-04-19 03:21:57 +0000

    add cat.c

---
+++ src/ulib/cat.c
@@ -0,0 +1,19 @@
+#include <const.h>
+#include <system.h>
+#include <fs.h>
+#include <file.h>
+
+int do_cat(char *path) {
+    int fd, n;
+    char buf[32];
+
+    fd = do_open(path, O_RDONLY, 0);
+    if (fd < 0) {
+        return -1;
+    }
+
+    while((n = do_read(fd, buf, 32)) > 0) {
+        do_write(0, buf, n);
+    }
+    do_close(fd);
+}
