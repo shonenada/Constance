6e901b76121f315d076c26eac832437603a1b388
shonenada <shonenada@gmail.com>
2015-04-24 09:35:19 +0000

    change the sched function in timer.c, have not test yet

--- src/kern/timer.c
+++ src/kern/timer.c
@@ -5,6 +5,7 @@
 #include <sched.h>
 
 static uint timer_ticks = 0;
+static uint wait_timer = 0;
 
 void set_timer_phaser(int hz) {
     int div = 1193180 / hz;
@@ -16,7 +17,7 @@ void set_timer_phaser(int hz) {
 int timer_handler(struct regs *r) {
     timer_ticks++;
     if (timer_ticks % TIMER_HZ == 0) {
-        schedule();
+        sched_cpu();
     }
     return 0;
 }
@@ -24,4 +25,10 @@ int timer_handler(struct regs *r) {
 void timer_init() {
     set_timer_phaser(TIMER_HZ);
     irq_install(0, timer_handler);
+}
+
+void timer_wait(int sec) {
+    long eticks;
+    eticks = timer_ticks + sec;
+    while(timer_ticks < eticks);
 }
